15d05f6d8cbbdd013d36d1c229df1c208f3f6348
Henco Burger <hencoburger@gmail.com>
2026-03-09 23:25:35 +1100

    Expose generic workers

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,8 +1,15 @@
 export { orchestrate } from "./orchestrator.js";
+export { orchestrateGeneric } from "./generic-orchestrator.js";
 export { aggregate, computeStats, aggregateGeneric } from "./aggregator.js";
 export { runClient } from "./load-client.js";
 export { runScript } from "./runtime.js";
 export { MetricsCollector } from "./metrics-collector.js";
+export { handleGenericRun } from "../worker/generic-handler.js";
+export type {
+  GenericWorkerRequest,
+  GenericWorkerResponse,
+} from "../worker/generic-handler.js";
+export type { DistributedConfig } from "./generic-orchestrator.js";
 export type {
   ClientConfig,
   ClientMetrics,
